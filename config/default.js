477a5dd38e8b05a5eac50508e7c56ce3e1c064c9
Enoch Klufio <enochmk94@gmail.com>
2021-11-09 15:30:09 +0000

    Folders created

---
+++ config/default.js
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config();
+const folderName = path.dirname(__dirname).split('/').pop();
+
+module.exports = {
+	appName: 'LUCIEBLUE',
+	port: process.env.PORT ?? 5006,
+	NODE_ENV: process.env.NODE_ENV ?? 'development',
+	DATABASE_URL: process.env.DATABASE_URL,
+	logs: {
+		windows: path.join('C:', 'logs', folderName),
+		linux: path.join('home', 'verification', 'logs', folderName),
+	},
+};
--- index.js
+++ index.js
@@ -3,8 +3,6 @@ const cors = require('cors');
 const morgan = require('morgan');
 const chalk = require('chalk');
 const dotenv = require('dotenv');
-const { PrismaClient } = require('@prisma/client');
-const prisma = new PrismaClient();
 
 const app = express();
 dotenv.config();
@@ -12,12 +10,6 @@ dotenv.config();
 app.use(cors());
 app.use(morgan('tiny'));
 app.use(express.json());
-
-app.get('/', async (req, res) => {
-	const data = await prisma.cust_orders.findMany();
-
-	res.send(data);
-});
 
 // Set server port to listen
 const PORT = process.env.PORT || 3000;
