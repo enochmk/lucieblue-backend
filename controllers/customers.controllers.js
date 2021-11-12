edb0c3f3f3f5cf621529af307cd7eaf6de876877
Paul Quartey <pandaddie@gmail.com>
2021-11-12 08:27:22 +0000

    updated controller for customers

--- controllers/customers.controllers.js
+++ controllers/customers.controllers.js
@@ -42,11 +42,11 @@ exports.deleteCustomer = asyncHandler(async (req, res, next) => {
     status: true,
     message: response,
   });
-
-
-
 });
 
+//
+// GET one customer from DB
+//
 exports.getCustomer = asyncHandler(async (req, res, next) => {
   const Customer = CustomerRepo();
 
@@ -60,7 +60,38 @@ exports.getCustomer = asyncHandler(async (req, res, next) => {
   });
 });
 
-
 /**
- * @description: update a Customer in the database
+ * @description: Get all users from the DB
  */
+exports.getCustomers = asyncHandler(async (req, res, next) => {
+  const Customer = CustomerRepo();
+
+  const result = await Customer.find();
+
+  return res.send({
+    status: true,
+    message: result,
+  });
+});
+
+exports.updateCustomer = asyncHandler(async (req, res, next) => {
+  const Customer = CustomerRepo();
+
+  const response = await Customer.update(req.params.id, {
+    FIRSTNAME: req.body.firstname,
+    SURNAME: req.body.lastname,
+    ADDRESS: req.body.address,
+    ALT_NUMBER: req.body.altNumber,
+    CITY: req.body.city,
+    COMMENTS: req.body.comments,
+    COUNTRY: req.body.country,
+    EMAIL: req.body.email,
+    PHONE_NUMBER: req.body.phoneNumber,
+
+  });
+
+  return res.send({
+    status: true,
+    message: response,
+  });
+});
--- routes/customers.route.js
+++ routes/customers.route.js
@@ -1,10 +1,12 @@
 const express = require('express');
-const { addCustomer, deleteCustomer, getCustomer } = require('../controllers/customers.controllers');
+const { addCustomer, deleteCustomer, getCustomer, getCustomers, updateCustomer } = require('../controllers/customers.controllers');
 
 const router = express.Router();
 
 router.route('/').post(addCustomer);
 
-router.route('/:id').delete(deleteCustomer).get(getCustomer);
+router.route('/').get(getCustomers);
+
+router.route('/:id').delete(deleteCustomer).get(getCustomer).put(updateCustomer);
 
 module.exports = router;
